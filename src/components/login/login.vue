9375583e58eb6864c406aee2ba29e244cd866b95
xlb <1500517473@qq.com>
2020-04-07 12:17:33 +0800

    vue-loginn

---
+++ src/components/login/login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>login Window</p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss"></style>>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import login from "@/components/login/login";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "login",
+      component: login
     }
   ]
-})
+});
